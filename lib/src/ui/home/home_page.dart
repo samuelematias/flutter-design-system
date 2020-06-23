fa6f9e31c737229d731a2d0f1f60db806b818e3f
samuelematias <hi@samuelematias.com>
2020-06-23 20:50:08 -0300

    add chrome icon on HomePage

--- lib/src/ui/home/home_page.dart
+++ lib/src/ui/home/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Home Page')),
-      body: Container(),
+      body: Center(
+        child: Icon(AntDesign.chrome),
+      ),
     );
   }
 }
