36e4d9762ec828865fe40e39605813b1669a1411
samuelematias <hi@samuelematias.com>
2020-06-23 22:14:22 -0300

    implemented Switch Dark Theme

--- lib/src/ui/overview/overview_page.dart
+++ lib/src/ui/overview/overview_page.dart
@@ -1,16 +1,25 @@
+import 'package:designsystem/src/core/storage/storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_icons/flutter_icons.dart';
+import 'package:provider/provider.dart';
 
 class OverviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final _settings = Provider.of<Settings>(context, listen: false);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Overview'),
         elevation: 0,
       ),
-      body: Center(
-        child: Icon(AntDesign.chrome),
+      body: ListView(
+        children: [
+          SwitchListTile(
+            title: Text('Dark mode'),
+            value: _settings.isDark,
+            onChanged: _settings.updateIsDark,
+          )
+        ],
       ),
     );
   }
