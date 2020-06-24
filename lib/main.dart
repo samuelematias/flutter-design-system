47444798147152f317f78e6e9deca25003ef4354
samuelematias <hi@samuelematias.com>
2020-06-23 22:13:58 -0300

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
+import 'package:designsystem/src/core/storage/storage.dart';
 import 'package:designsystem/src/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  Settings _settings;
+
+  @override
+  void initState() {
+    _settings = Settings();
+    _settings.init();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _settings?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Design System',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: CustomTabBar(),
+    return MultiProvider(
+      providers: [
+        Provider<Settings>.value(value: _settings),
+      ],
+      child: StreamBuilder<Settings>(
+          stream: _settings.stream,
+          builder: (context, snapshot) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: _settings.isFresh,
+              title: 'Design System',
+              theme: ThemeData(
+                primarySwatch: Colors.deepPurple,
+                visualDensity: VisualDensity.adaptivePlatformDensity,
+              ),
+              darkTheme: ThemeData.dark(),
+              themeMode: _settings.isDark ? ThemeMode.dark : ThemeMode.light,
+              home: CustomTabBar(),
+            );
+          }),
     );
   }
 }
