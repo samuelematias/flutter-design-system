c07cb1b739a31682e3645e7bac0d41c4d40e29c8
samuelematias <hi@samuelematias.com>
2020-06-25 20:14:39 -0300

    create IntroductionPage

---
+++ lib/src/ui/overview/introduction/introduction_page.dart
@@ -0,0 +1,18 @@
+import 'package:designsystem/src/ui/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class IntroductionPage extends StatelessWidget {
+  final bool hideSwitchMode;
+
+  const IntroductionPage({
+    this.hideSwitchMode = false,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return CustomAppBar(
+      pageTitle: 'Introduction',
+      hideSwitchMode: hideSwitchMode,
+      child: Center(child: Text('Olá Introduction!')),
+    );
+  }
+}
