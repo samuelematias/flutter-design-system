a3a8636a30ec135f76ad63d62f1ba0d4a393e161
samuelematias <hi@samuelematias.com>
2020-06-25 19:28:23 -0300

    implemented CustomAppBar on Screens

--- lib/src/ui/components/components_page.dart
+++ lib/src/ui/components/components_page.dart
@@ -1,15 +1,13 @@
+import 'package:designsystem/src/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class ComponentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Components'),
-        elevation: 0,
-      ),
-      body: Center(
+    return CustomAppBar(
+      pageTitle: 'Components',
+      child: Center(
         child: Icon(AntDesign.apple1),
       ),
     );
