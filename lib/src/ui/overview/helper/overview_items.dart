628502be95d08923788e9e914da3a1147c579780
samuelematias <hi@samuelematias.com>
2020-06-25 22:16:51 -0300

    update OverviewItems

--- lib/src/ui/overview/helper/overview_items.dart
+++ lib/src/ui/overview/helper/overview_items.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
 class Overview {
+  final String id;
+  final IconData icon;
   final String title;
   final String subtitle;
 
-  Overview(this.title, this.subtitle);
+  Overview(this.id, this.icon, this.title, this.subtitle);
 }
 
 List<Overview> overviewItems() {
   return [
-    Overview('Introduction', 'A brief introduction to the design system'),
+    Overview(
+      '1',
+      AntDesign.book,
+      'Introduction',
+      'A brief introduction to the design system',
+    ),
   ];
 }
