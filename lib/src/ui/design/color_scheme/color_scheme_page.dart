21eb78d7adb8284c4f1f4201c717926a080067f9
samuelematias <hi@samuelematias.com>
2020-06-28 17:39:55 -0300

    implemented colorList on ColorSchemePage

--- lib/src/ui/design/color_scheme/color_scheme_page.dart
+++ lib/src/ui/design/color_scheme/color_scheme_page.dart
@@ -1,3 +1,4 @@
+import 'package:designsystem/src/ui/design/color_scheme/helper/colors_items.dart';
 import 'package:designsystem/src/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:designsystem/src/core/theme/theme.dart';
@@ -10,54 +11,65 @@ class ColorSchemePage extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    // final List<ColorsItems> _items = colorsItems();
-
-    return CustomAppBar(
-      pageTitle: 'Color Scheme',
-      hideSwitchMode: hideSwitchMode,
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SelectableText('Color Scheme').h1(),
-            Padding(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SelectableText('Color Palette').h2(),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 16.0),
-                    child: SelectableText(
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.')
-                        .h2(style: TextStyle(fontWeight: FontWeight.w200)),
+  Widget build(BuildContext context) => CustomAppBar(
+        pageTitle: 'Color Scheme',
+        hideSwitchMode: hideSwitchMode,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: SelectableText('Color Scheme').h1(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.0),
+                        child: SelectableText('Color Palette').h2(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: SelectableText(
+                                'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.')
+                            .h2(style: TextStyle(fontWeight: FontWeight.w200)),
+                      ),
+                      _buildColorList(),
+                    ],
                   ),
-                  // Container(
-                  //   height: 200,
-                  //   child: ListView.builder(
-                  //     scrollDirection: Axis.horizontal,
-                  //     itemCount: _items.length,
-                  //     itemBuilder: (context, index) {
-                  //       final _item = _items[index];
-                  //       return ColorDemonstration(
-                  //         colorName: _item.colorName,
-                  //         colorHex: _item.colorHex,
-                  //       );
-                  //     },
-                  //   ),
-                  // ),
-                  ColorDemonstration(
-                    colorName: 'Dark Blue',
-                    colorHex: '#009EEB',
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
+      );
+
+  Widget _buildColorList() {
+    return Container(
+      height: 400,
+      child: GridView.count(
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        crossAxisCount: 3,
+        mainAxisSpacing: 30.0,
+        padding: EdgeInsets.only(left: 16.0, right: 16.0),
+        children: _buildContentList(),
       ),
     );
   }
+
+  List<Widget> _buildContentList() {
+    final List<ColorsItems> items = colorsItems();
+    return items.map((item) => _buildContentListItem(item)).toList();
+  }
+
+  Widget _buildContentListItem(ColorsItems item) => ColorDemonstration(
+        colorName: item.colorName,
+        colorHex: item.colorHex,
+      );
 }
