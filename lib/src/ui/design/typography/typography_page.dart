c0b1385ffe8c3bdf6de4a6d3b49afea93588858f
samuelematias <hi@samuelematias.com>
2020-06-28 18:22:57 -0300

    starting implement TypographyPage

--- lib/src/ui/design/typography/typography_page.dart
+++ lib/src/ui/design/typography/typography_page.dart
@@ -1,5 +1,6 @@
 import 'package:designsystem/src/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:designsystem/src/core/theme/theme.dart';
 
 class TypographyPage extends StatelessWidget {
   final bool hideSwitchMode;
@@ -13,7 +14,42 @@ class TypographyPage extends StatelessWidget {
     return CustomAppBar(
       pageTitle: 'Typography',
       hideSwitchMode: hideSwitchMode,
-      child: Center(child: Text('Hello, Typography!')),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: SelectableText('Typography').h1(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: SelectableText('Font sizes').h2(),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: SelectableText(
+                              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.')
+                          .h2(style: TextStyle(fontWeight: FontWeight.w200)),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: HighlightCode(code: '''Text('Pomodoro').h1()'''),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
