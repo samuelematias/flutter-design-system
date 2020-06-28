af2a0f4803a67279c469fdabe7ffc64da51f7001
samuelematias <hi@samuelematias.com>
2020-06-28 18:21:16 -0300

    create HighlightCode widget

---
+++ lib/src/ui/widgets/highlight/highlight.dart
@@ -0,0 +1,3 @@
+library highlight;
+
+export 'highlight_code.dart';
--- lib/src/ui/widgets/widgets.dart
+++ lib/src/ui/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'adaptive/adaptive.dart';
 export 'app_bar/app_bar.dart';
 export 'item_tile/item_tile.dart';
 export 'tab/tab.dart';
+export 'highlight/highlight.dart';
