040839866b73349082b8e2dbebde9e5cf990483d
samuelematias <hi@samuelematias.com>
2020-07-04 23:17:16 -0300

    Removed DarkMode Switch from CustomAppBar

--- lib/src/ui/widgets/app_bar/custom_app_bar.dart
+++ lib/src/ui/widgets/app_bar/custom_app_bar.dart
@@ -1,42 +1,28 @@
-import 'package:day_night_switcher/day_night_switcher.dart';
-import 'package:designsystem/src/core/storage/storage.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String pageTitle;
   final Widget child;
   final GestureTapCallback onClickBackButton;
   final bool hideSwitchMode;
+  final List<Widget> actions;
 
   const CustomAppBar({
     @required this.pageTitle,
     @required this.child,
     this.onClickBackButton,
     this.hideSwitchMode = false,
+    this.actions,
   });
   @override
   Widget build(BuildContext context) {
-    final _settings = Provider.of<Settings>(context, listen: false);
-
     return WillPopScope(
       onWillPop: () async => _onWillPop(context),
       child: Scaffold(
         appBar: AppBar(
           title: SelectableText(pageTitle),
           elevation: 0,
-          actions: [
-            Visibility(
-              visible: !hideSwitchMode,
-              child: Padding(
-                padding: EdgeInsets.only(right: 16.0),
-                child: DayNightSwitcherIcon(
-                  isDarkModeEnabled: _settings.isDark,
-                  onStateChanged: _settings.updateIsDark,
-                ),
-              ),
-            ),
-          ],
+          actions: actions,
         ),
         body: child,
       ),
